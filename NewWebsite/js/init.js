521700330fab72286036493f2d4de135eafe11d4
Thúlio Balbuena <thuliobalbuena@hotmail.com>
2021-08-18 23:13:23 -0300

    Inserir autenticação com o firebase e melhorar interface

---
+++ NewWebsite/js/init.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBDpGMLaahW2hx5MWM-Udi8bvKlt95eTsI",
+  authDomain: "ishower.firebaseapp.com",
+  databaseURL: "https://ishower-default-rtdb.firebaseio.com",
+  projectId: "ishower",
+  storageBucket: "ishower.appspot.com",
+  messagingSenderId: "790343473441",
+  appId: "1:790343473441:web:3f68e52677aa2e79794660",
+  measurementId: "G-7V1ZC28PJ7"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
