e9849aeca06ace8cf3b00191d5db5a3e3c8ed99b
Thúlio Balbuena <thuliobalbuena@hotmail.com>
2021-10-14 17:25:24 -0300

    Resolver problemas

--- NewWebsite/js/database.js
+++ NewWebsite/js/database.js
@@ -1,12 +1,14 @@
 var dadoss = document.getElementById('dadoss');
 var dbRef  = firebase.database().ref().child('Volume');
-dbRef.on('value', snap => dadoss.innerText = snap.val());
+dbRef.on('value', snap => dadoss.innerText = snap.val() + ' Litros');
 
 var tempo = document.getElementById('tempo');
 var dbRef2  = firebase.database().ref().child('Tempo');
-dbRef2.on('value', snap => tempo.innerText = snap.val());
+dbRef2.on('value', snap => tempo.innerText = snap.val() + ' Minutos');
 
 var preco = document.getElementById('preco');
 var dbRef3  = firebase.database().ref().child('Preço');
-dbRef3.on('value', snap => preco.innerText = snap.val());
+dbRef3.on('value', snap => preco.innerText = 'R$:' + snap.val());
+
+
 
